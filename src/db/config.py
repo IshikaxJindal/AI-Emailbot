721550548e04459feccd3521022a3d4a646e9379
aaradhya1205 <guptaaaradhya3@gmail.com>
2026-03-30 19:20:59 +0530

    Update config.py

--- src/db/config.py
+++ src/db/config.py
@@ -1,1 +1,27 @@
+# db/config.py
 
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,
+    max_overflow=20,
+    echo=False
+)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
